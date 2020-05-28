f252e2da19b2b35a9d47e67eaa97a744af8fcf37
gowtham <Gowtham.Varadharaj@srinsofttech.lan>
2020-05-28 15:35:02 +0530

    varatha added and modified

--- src/test/java/org/git/Git.java
+++ src/test/java/org/git/Git.java
@@ -5,7 +5,9 @@ public class Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hi");
+		System.out.println("hey");
+		System.out.println("varatharaj");
+		System.out.println("new line");
 
 	}
 
